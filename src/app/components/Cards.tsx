895b573fd389730fb86474d742f9fa8c71c10c41
LucasE. <glucaseduardo@gmail.com>
2025-09-09 14:28:05 -0300

    like

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,26 +1,37 @@
+'use client'
 import { cafezinho } from "../data/Produtos";
+import useAlert from "../hooks/useAlerts";
 import "./Cards.css";
+import HeartLike from "./HeartLike";
 
 export default function Cards() {
-    return(
-        <section className="cards-section">
-            <h1 className="cards-title">CAFÉS</h1>
-            <main className="cards-container">
-                {cafezinho.map((product, index)=>(
-                    <div key={index} className="card">
-                        <div className="card-image-wrapper">
-                            <img className="card-image" src={product.image} alt={product.title} />
-                        </div>
-                        <div className="card-content">
-                            <div>
-                                <h3 className="card-title">{product.title}</h3>
-                                <p className="card-description">{product.description}</p>
-                            </div>
-                            <button className="card-button">Comprar : {product.price}</button>
-                        </div>
-                    </div>
-                ))}
-            </main>
-        </section>
-    )
+    const {ConfirmAlert, doubleAlert}=useAlert();
+  return (
+    <section className="cards-section">
+      <h1 className="cards-title">CAFÉS</h1>
+      <main className="cards-container">
+        {cafezinho.map((product, index) => (
+          <div key={index} className="card">
+            <div className="card-image-wrapper">
+              <img
+                className="card-image"
+                src={product.image}
+                alt={product.title}
+              />
+            </div>
+            <div className="card-content">
+              <div>
+                <div className="texto-btn">
+                  <h3 className="card-title">{product.title}</h3>
+                  <HeartLike initialCount={product.like}/>
+                </div>
+                <p className="card-description">{product.description}</p>
+              </div>
+              <button className="card-button"onClick={()=>doubleAlert()}>Comprar : {product.price}</button>
+            </div>
+          </div>
+        ))}
+      </main>
+    </section>
+  );
 }
--- src/app/data/Produtos.ts
+++ src/app/data/Produtos.ts
@@ -4,23 +4,27 @@ export const cafezinho = [
         description: "Grãos de café puro, concentrados num sabor artesanal",
         price: "R$ 10,00",
         image: "cafe1.png",
+        like:10
     },
     {
         title: "Capuccino",
         description: "Café artesanal batido com leite e cobertura de chantilly",
         price: "R$ 12,00",
         image: "cafe2.png",
+        like:10
     },
     {
         title: "Café Gelado",
         description: "Café puro batido com leite, adicionado de cubos de gelo",
         price: "R$ 12,00",
         image: "cafe3.png",
+        like:10
     },
     {
         title: "Choccafé",
         description: "Mistura dos grãos artesanais de café com chocolate 70%",
         price: "R$ 13,00",
         image: "cafe4.png",
+        like:10
     },
 ]
